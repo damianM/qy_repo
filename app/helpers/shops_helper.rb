628be8901f556f4b27146591936a97f442cd1f74
Damian Marciniec <damian.marciniec@openetworks.pl>
2010-10-08 00:28:17 +0200

    google map for shops

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,2 +1,6 @@
 module ShopsHelper
+
+  def shop_address shop
+    shop.street + ', ' + shop.city + ', Polska'
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,31 +11,4 @@ function checkScreen(){
 }
 
 
-function showGoogleMap(address) {
-  alert(address);  
-  if (GBrowserIsCompatible()) {
-    var map = new GMap2(document.getElementById("map"));
-    var geocoder = new GClientGeocoder();
-    map.setUIToDefault();
 
-
-    geocoder.getLatLng(
-      address,
-      function(point) {
-        if (!point) {
-          alert(address + " not found");
-        } else {
-          map.setCenter(point, 13);
-          var marker = new GMarker(point);
-          map.addOverlay(marker);
-          var companyDetailsHtml = "<div style='white-space:nowrap; font-family: Verdana, Tahoma, Arial, sans-serif; color: #154141; font-size: 1em;'>Firma,<br/>"+address+"</div>";
-          GEvent.addListener(marker, 'click', function() {
-            marker.openInfoWindowHtml(companyDetailsHtml);
-          });
-          marker.openInfoWindowHtml(address);
-          GEvent.trigger(marker, 'click');
-        }
-      }
-    );
-  }
-}
