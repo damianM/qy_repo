a2202303187d93f841f47128a7075c9704ce74d9
Damian Marciniec <damian.marciniec@openetworks.pl>
2010-10-08 00:47:47 +0200

    gm

--- public/javascripts/google_maps.js
+++ public/javascripts/google_maps.js
@@ -1,7 +1,6 @@
 var GoogleMaps = $.inherit({
   __constructor : function() {
     $('#map').livequery($.delegate(this, this.showGoogleMap));
-    $('#direction_window').livequery('click',$.delegate(this, this.openDirectionWindow));
   },
 
   showGoogleMap: function(src) { 
@@ -22,7 +21,7 @@ var GoogleMaps = $.inherit({
             map.setCenter(point, 13);
             var marker = new GMarker(point);
             map.addOverlay(marker);
-            var companyDetailsHtml = "<div style='white-space:nowrap; font-family: Verdana, Tahoma, Arial, sans-serif; color: #154141; font-size: 1em;'>"+company_name+"<br/>"+address+"<br/><a id='direction_window' href='#' address='"+address+"'>zobacz kierunki</a></div>";
+            var companyDetailsHtml = "<div style='white-space:nowrap; font-family: Verdana, Tahoma, Arial, sans-serif; color: #154141; font-size: 1em;'>"+company_name+"<br/>"+address+"<br/><a href='#' onClick='openDirectionWindow();'>zobacz kierunki</a></div>";
 
             GEvent.addListener(marker, 'click', function() {
               marker.openInfoWindowHtml(companyDetailsHtml);
@@ -33,13 +32,7 @@ var GoogleMaps = $.inherit({
         }
       );
     }
-  },
-
-	    
-  openDirectionWindow: function(src) {
-    var address = $(src).attr('address');
-    openPopupWindow("http://maps.google.com/maps?daddr="+encodeURI(address.replace(/ /g,"+"))+"&hl=pl");
-  }
+  }	   
 
 }, {
 	
@@ -49,6 +42,11 @@ jQuery(document).ready(function() {
   new GoogleMaps();
 });
 
+  
+function openDirectionWindow(){
+  var address = $('#map').attr('address');
 
+  openPopupWindow("http://maps.google.com/maps?daddr="+encodeURI(address.replace(/ /g,"+"))+"&hl=pl");
+}
 
 
