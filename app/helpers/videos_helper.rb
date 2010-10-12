c641e237634029ba0e90e8139ab546b26536d99b
michal <michal.lydka@gmail.com>
2010-10-13 00:04:59 +0200

    video - przywracanie pierwotnej wersji progress bara

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,2 +1,9 @@
 module VideosHelper
+
+  def add_video_button label
+    link_to_function label do |page|
+      page.replace_html :add_form, :partial => 'videos/new', :locals => { :video => Video.new }
+    end
+  end
+
 end
