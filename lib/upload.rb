65f6867efa2a24d784b1d884d7c488166fca0b8f
root <root@quadomaniacy.pl>
2010-11-13 09:18:06 +0100

    changes

--- lib/upload.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'fastthread'
-require 'drb'
-require 'gem_plugin'
-GemPlugin::Manager.instance.load 'mongrel' => GemPlugin::INCLUDE
-DRb.start_service 'druby://127.0.0.1:2999', Mongrel::UploadProgress.new
-DRb.thread.join
-
