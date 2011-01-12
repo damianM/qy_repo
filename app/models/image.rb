b409713c9ed5850cceab7aa07947b97e772b600b
michal <michal.lydka@gmail.com>
2011-01-13 00:28:56 +0100

    upload zdjec

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,14 @@
 class Image < Asset
+  require 'RMagick'
 
-  Image.attachment_options.merge!( :content_type => [ 'image/jpeg', 'image/jpg', 'image/gif','image/png'],
-                                        :path_prefix  => '/public/uploads/' + self.name.to_s.downcase.pluralize,
-                                        :thumbnail_class => Image, :resize_to => [242, 151] )
+  Image.attachment_options.merge!( :content_type => Technoweenie::AttachmentFu.content_types,
+                                   :path_prefix  => '/public/uploads/' + self.name.to_s.downcase.pluralize,
+                                   :processor => :Rmagick,
+                                   :max_size => 3.megabytes,
+                                   :resize_to => [242, 151],
+                                   :thumbnails => { :thumb => [93, 71] },
+                                   :thumbnail_class => Image )
+
 
   def flash?
     content_type == 'application/x-shockwave-flash'
