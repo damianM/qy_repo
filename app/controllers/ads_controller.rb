624c63d46da5791cb96702f754809e5e7b230783
michal <michal.lydka@gmail.com>
2011-01-20 23:59:45 +0100

    widok searcha

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -8,53 +8,9 @@ class AdsController < ApplicationController
     @special_ads = SaleAd.find(:all, :conditions => {:special => true})
   end
   
-  def index
-    @ad = Ad.all
-  end
-  
-  def new
-    @ad = Ad.new
-    @ad.serial = Ad.generate_serial
+  def search
+
   end
 
-  def create
-    @ad = Ad.new(params[:ad])
 
-    if @ad.save
-      flash[:notice] = "Ogłoszenie zostało dodana."
-
-      redirect_to ads_path
-    else
-      flash[:error]  = "Próba utworzenia ogłoszenia nie powiodła się."
-      render :action => 'new'
-    end
-  end
-  
-  def show
-    @ad = Ad.find(params[:id])
-  end
-  
-  def edit
-    @ad = Ad.find(params[:id])
-    @ad.serial = Ad.generate_serial
-  end
-
-  def update
-    @ad = Ad.find(params[:id])
-
-    if @ad.update_attributes(params[:ad])
-      flash[:notice] = "Edycja przebiegła pomyślnie."
-      redirect_to ads_path
-    else
-      flash[:error]  = "Edycja nie powiodła się."
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    @ad = Ad.find(params[:id])
-    @ad.destroy
-    flash[:notice] = "Ogłoszenie zostało usuniete."
-  end
-  
 end
